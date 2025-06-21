748a1595d7a8aa97f9a7aa8886eb29a67f5343d8
melio <dhitanimam05@gmail.com>
2025-06-21 20:59:50 +0700

    feat: Update Profile Page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,46 +1,86 @@
 import { Link } from "react-router-dom";
 import { useAuthStore } from "../store/useAuthStore.js";
-import { LogOut, MessageSquare, Settings, User } from "lucide-react";
+import { LogOut, MessageSquare, Settings, User, Menu } from "lucide-react"; // Imported Menu icon
 
 const Navbar = () => {
   const { logout, authUser } = useAuthStore();
   return (
-    <header className="fixed top-0 z-40 w-full border-b bg-base-100 border-base-300 backdrop-blur-lg bg-base-100/80">
-      <div className="container h-16 px-4 mx-auto">
-        <div className="flex items-center justify-between h-full">
-          <Link
-            to="/"
-            className="flex items-center gap-2.5 hover:opacity-80 transition-all"
-          >
-            <div className="flex items-center justify-center rounded-lg size-9 bg-primary/10">
-              <MessageSquare className="w-5 h-5 text-primary" />
-            </div>
-            <h1 className="text-lg font-bold">ZUNO</h1>
-          </Link>
-          <div className="flex items-center gap-2">
+    <>
+      <header className="fixed top-0 z-40 w-full border-b bg-base-100 border-base-300 backdrop-blur-lg bg-base-100/80">
+        <div className="container h-16 px-4 mx-auto">
+          <div className="flex items-center justify-between h-full">
             <Link
-              to={"/settings"}
-              className={`btn btn-sm gap-2 transition-colors`}
+              to="/"
+              className="flex items-center gap-2.5 hover:opacity-80 transition-all"
             >
-              <Settings className="size-4" />
-              <span className="hidden sm:inline">Settings</span>
+              <div className="flex items-center justify-center rounded-lg size-9 bg-primary/10">
+                <MessageSquare className="w-5 h-5 text-primary" />
+              </div>
+              <h1 className="text-lg font-bold">ZUNO</h1>
             </Link>
-            {authUser && (
-              <>
-                <Link to="/profile" className={`btn btn-sm gap-2`}>
-                  <User className="size-5" />
-                  <span className="hidden sm:inline">Profile</span>
-                </Link>
-                <button className="flex items-center gap-2" onClick={logout}>
-                  <LogOut className="size-5" />
-                  <span className="hidden sm:inline">Logout</span>
-                </button>
-              </>
-            )}
+            <div className="md:hidden">
+              <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn btn-ghost btn-circle">
+                  <Menu className="size-5" />
+                </label>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
+                >
+                  <li>
+                    <Link to={"/settings"}>Settings</Link>
+                  </li>
+                  {authUser && (
+                    <>
+                      <li>
+                        <Link to="/profile">Profile</Link>
+                      </li>
+                      <li>
+                        <button onClick={logout}>Logout</button>
+                      </li>
+                    </>
+                  )}
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+      <nav className="fixed z-40 flex-col hidden gap-2 p-2 transition-all duration-300 -translate-y-1/2 border border-l-0 rounded-r-lg left-5 group md:flex top-1/2 border-base-300 bg-base-100/80 backdrop-blur-lg hover:shadow-lg">
+        <Link
+          to={"/settings"}
+          className="flex items-center p-2 transition-colors rounded-lg hover:bg-base-200"
+        >
+          <Settings className="size-5 shrink-0" />
+          <span className="w-0 overflow-hidden text-sm font-medium transition-all duration-300 opacity-0 group-hover:w-auto group-hover:opacity-100 group-hover:ml-3">
+            Settings
+          </span>
+        </Link>
+
+        {authUser && (
+          <>
+            <Link
+              to="/profile"
+              className="flex items-center p-2 transition-colors rounded-lg hover:bg-base-200"
+            >
+              <User className="size-5 shrink-0" />
+              <span className="w-0 overflow-hidden text-sm font-medium transition-all duration-300 opacity-0 group-hover:w-auto group-hover:opacity-100 group-hover:ml-3">
+                Profile
+              </span>
+            </Link>
+            <button
+              onClick={logout}
+              className="flex items-center p-2 transition-colors rounded-lg hover:bg-base-200"
+            >
+              <LogOut className="size-5 shrink-0" />
+              <span className="w-0 overflow-hidden text-sm font-medium transition-all duration-300 opacity-0 group-hover:w-auto group-hover:opacity-100 group-hover:ml-3">
+                Logout
+              </span>
+            </button>
+          </>
+        )}
+      </nav>
+    </>
   );
 };
 
--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,7 +1,165 @@
-import React from "react";
+import { AtSign, Camera, Loader2, Mail, User } from "lucide-react";
+import { useAuthStore } from "../store/useAuthStore";
+import { useState } from "react";
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [formData, setFormData] = useState({
+    fullName: authUser.fullName || "",
+    username: authUser.username || "",
+    profilePic: "",
+  });
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      const base64Image = reader.result;
+      setSelectedImage(base64Image);
+      setFormData((prev) => ({ ...prev, profilePic: base64Image }));
+    };
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    updateProfile(formData);
+  };
+
+  return (
+    <div className="h-screen pt-20">
+      <div className="max-w-2xl p-4 py-8 mx-auto">
+        <div className="p-6 space-y-5 bg-base-300 rounded-xl">
+          <div className="text-center">
+            <h1 className="text-2xl font-semibold">Profile</h1>
+            <p className="mt-2">Your Account Information</p>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col items-center gap-4">
+              <div className="relative">
+                <img
+                  src={selectedImage || authUser.profilePic || "/avatar.png"}
+                  alt="Profile"
+                  className="object-cover border-4 rounded-full size-32"
+                />
+                <label
+                  htmlFor="avatar-upload"
+                  className={`
+                absolute bottom-0 right-0
+                bg-base-content hover:scale-105
+                p-2 rounded-full cursor-pointer
+                transition-all duration-200
+                ${isUpdatingProfile ? "animate-pulse pointer-events-none" : ""}
+                `}
+                >
+                  <Camera className="w-5 h-5 text-base-200" />
+                  <input
+                    type="file"
+                    id="avatar-upload"
+                    className="hidden"
+                    accept="image/*"
+                    onChange={handleImageUpload}
+                    disabled={isUpdatingProfile}
+                  />
+                </label>
+              </div>
+              <p className="text-sm text-zinc-400">
+                {isUpdatingProfile
+                  ? "Uploading..."
+                  : "Click the camera icon to update your profile picture"}
+              </p>
+            </div>
+            <div className="space-y-6">
+              <div className="space-y-1.5">
+                <label className="label">
+                  <span className="font-medium label-text">Full Name</span>
+                </label>
+                <div className="relative">
+                  <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <User className="size-5 text-base-content/40" />
+                  </div>
+                  <input
+                    type="text"
+                    className={`input input-bordered w-full pl-10`}
+                    value={formData.fullName}
+                    onChange={(e) =>
+                      setFormData({ ...formData, fullName: e.target.value })
+                    }
+                  />
+                </div>
+              </div>
+              <div className="space-y-1.5">
+                <label className="label">
+                  <span className="font-medium label-text">Username</span>
+                </label>
+                <div className="relative">
+                  <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <AtSign className="size-5 text-base-content/40" />
+                  </div>
+                  <input
+                    type="text"
+                    className={`input input-bordered w-full pl-10`}
+                    value={formData.username}
+                    onChange={(e) =>
+                      setFormData({ ...formData, username: e.target.value })
+                    }
+                  />
+                </div>
+              </div>
+              <div className="space-y-1.5">
+                <label className="label">
+                  <span className="font-medium label-text">Email</span>
+                </label>
+                <div className="relative">
+                  <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <Mail className="size-5 text-base-content/40" />
+                  </div>
+                  <input
+                    type="email"
+                    className={`input input-bordered w-full pl-10`}
+                    value={authUser.email}
+                    disabled
+                    onChange={(e) =>
+                      setFormData({ ...formData, email: e.target.value })
+                    }
+                  />
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="w-full btn btn-primary"
+                disabled={isUpdatingProfile}
+              >
+                {isUpdatingProfile ? (
+                  <>
+                    <Loader2 className="size-5 animate-spin" />
+                    Loading...
+                  </>
+                ) : (
+                  "Update Profile"
+                )}
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="p-6 mt-6 bg-base-300 rounded-xl">
+          <h2 className="mb-4 text-lg font-medium">Account Informatiom</h2>
+          <div className="space-y-3 text-sm">
+            <div className="flex items-center justify-between py-2 border-b border-zinc-700">
+              <span>Member Since</span>
+              <span>{authUser.createdAt?.split("T")[0]}</span>
+            </div>
+            <div className="flex items-center justify-between py-2">
+              <span>Account Status</span>
+              <span className="text-green-500">Active</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
